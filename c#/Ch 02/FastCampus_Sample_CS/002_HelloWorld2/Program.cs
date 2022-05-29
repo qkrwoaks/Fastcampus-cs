df1690c6068858b980fd9949d9c4a66821a514b7
qkrwoaks <qkrwoaks5327@naver.com>
2022-05-29 18:37:25 +0900

    ch 02. 처음으로 코딩하기 - 02. 코드 리뷰해보기 - 2
    -002_HelloWorld2, 003_HelloWorld3, 005_Check, 005_Check 솔루션 생성
    -Main 함수, Console을 통한 입출력에 대해 알아봄

---
+++ c#/Ch 02/FastCampus_Sample_CS/002_HelloWorld2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*---------------------------------------------------------------------------
+ * Name : _001_HelloWorld
+ * DESC : 메인 함수의 파라미터 보기
+--------------------------------------------------------------------------- */
+
+namespace _002_HelloWorld2
+{
+    class Program
+    {
+        // Main의 args에 값을 넣는 방법
+        // cmd 창을 열어 cd..을 입력한다.
+        // "cd"+ 파일 위치를 입력한다.
+        static void Main(string[] args)
+        {
+            if (args.Length > 0) // 입력받은 문자열이 0보다 클때(존재)
+            {
+                Console.WriteLine("HelloWorld:  " + args[0]);
+                // 콘솔 창에 "HelloWorld: args.Length = 0"라고 나타난다.
+                // 줄바꿈이 일어난다.
+            }
+            else
+            {
+                Console.WriteLine("HelloWorld: args.Length = 0");
+            }
+
+            Console.ReadKey(); // 콘솔창에서 Key값을 입력 받는다.
+        }
+    }
+}
---
+++ c#/Ch 02/FastCampus_Sample_CS/003_HelloWorld3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_HelloWorld3
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine(" static void Main(string[] args)");
+        //    // 콘솔 창에 " static void Main(string[] args)"라고 나타난다
+
+        //}
+
+        //static void Main()
+        //{
+        //    Console.WriteLine("static void Main");
+        //    // 콘솔 창에 "static void Main"라고 나타난다.
+        //    // 줄바꿈이 일어난다.
+        //}
+
+        //static int Main()
+        //{
+        //    Console.WriteLine("static int Main");
+        //    // 콘솔 창에 "static int Main"라고 나타난다.
+        //    // 줄바꿈이 일어난다.
+        //    return 0;
+        //    // 0을 반환한다.
+        //}
+
+        static int Main(string[] args)
+        {
+            Console.WriteLine("AAA{0} BBB{1}", 1, "ggg");
+            // 콘솔 창에 "AAA1 BBBggg"라고 나타난다.
+            // 줄바꿈이 일어난다.
+            Console.Write("{0}{1}{2}", 1, 2, 3);
+            // 콘솔 창에 "1, 2, 3"라고 나타난다.
+            Console.WriteLine("static int Main(string[] args)");
+            // 콘솔 창에 "static int Main(string[] args)"라고 나타난다.
+            return 0;
+            // 0을 반환한다.
+        }
+    }
+}
