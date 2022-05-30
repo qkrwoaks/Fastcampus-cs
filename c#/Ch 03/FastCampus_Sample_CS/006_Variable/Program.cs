e84ede29c6853422a45f35f866bb48e356c65b8c
qkrwoaks <82390527+qkrwoaks@users.noreply.github.com>
2022-05-30 23:12:44 +0900

    Ch 03. 데이터란 무엇일까 - 01. 기본 데이터1 - 1
    -FastCampus_Sample_CS 프로젝트 생성, 006_Variable, 007_Variable2 솔루션 생성
    -기본 정수 데이터형에 대해 알아봄

---
+++ c#/Ch 03/FastCampus_Sample_CS/006_Variable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*---------------------------------------------------------------------------
+ * Name : _006_Variable
+ * DESC : 변수 선언에 기초
+--------------------------------------------------------------------------- */
+
+namespace _006_Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num; // int(정수)형 변수 선언
+            num = 1000; // 변수 값 할당
+            Console.WriteLine(num);
+            // 콘솔에 "1000"라고 출력됨.
+        }
+    }
+}
---
+++ c#/Ch 03/FastCampus_Sample_CS/007_Variable2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*---------------------------------------------------------------------------
+ * Name : _007_Variable2
+ * DESC : 변수 선언에 다양한 방법..
+--------------------------------------------------------------------------- */
+
+namespace _007_Variable2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 100; // 선언과 동시에 초기화
+            int y = 200;
+            int a = 1000, b = 2000;
+
+            Console.WriteLine("x : {0}", x); // 콘솔에 "x: 100" 라고 출력됨
+            Console.WriteLine("y : " + y); // 콘솔에 "y: 200" 라고 출력됨
+            Console.WriteLine("a : " + a); // 콘솔에 "a: 1000" 라고 출력됨
+            Console.WriteLine("b : " + b); // 콘솔에 "b: 2000" 라고 출력됨
+        }
+    }
+}
