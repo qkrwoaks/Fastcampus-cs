70f6161a9e4c6c6e9945db09efd67df88272983a
qkrwoaks <82390527+qkrwoaks@users.noreply.github.com>
2022-06-02 22:51:32 +0900

    Ch 03. 데이터란 무엇일까 - 04. 기본데이터2 - 1
    -013_Data6, 014_Data7, 015_Data8, 016_String 솔루션 생성
    -소수점, 문자열 데이터에 대해 알아봄

---
+++ c#/Ch 03/FastCampus_Sample_CS/013_Data6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*---------------------------------------------------------------------------
+ * Name : _013_Data6
+ * DESC : 소수점 데이터형의 기초, 리터럴 접미사(f(F), m(M))
+--------------------------------------------------------------------------- */
+
+namespace _013_Data6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 3.141592312319273891237891f; // float형 뒤에는 리터럴 접미사(f)를 써야함.
+            Console.WriteLine("a : {0}", a); // 콘솔에 "a: 3.141592"라고 출력됨.
+
+            double b = 3.141592312319273891237891; // doble형 뒤에는 리터럴 접미사를 쓰지 않음.
+            Console.WriteLine("b : {0}", b); // 콘솔에 "b: 3.14159231231927"라고 출력됨.
+
+            decimal c = 3.141592312319273891237891m; // decimal형 뒤에는 리터럴 접미사(m)를 써야함.
+            Console.WriteLine("c : {0}", c); // 콘솔에 "c: 3.141592312319273891237891"라고 출력됨.
+        }
+    }
+}
---
+++ c#/Ch 03/FastCampus_Sample_CS/016_Data9/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*---------------------------------------------------------------------------
+ * Name : _016_String
+ * DESC : 문자열 제어
+--------------------------------------------------------------------------- */
+
+namespace _016_Data9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "abcdefg";
+            string str2 = "hijklmn";
+            string str3 = str1 + str2; // string 끼리는 "+"를 통해 더 할 수 있음.
+
+            Console.WriteLine("str1 + str2 : {0}", str3);
+            // 콘솔에 "str1 + str2 : abcdefghijklmn"라고 출력됨
+
+            string str4 = "zzz";
+            string str5 = "zzz";
+            Console.WriteLine("str4 == str5 : {0}", (str4 == str5));
+            // 콘솔에 "str4 == str5 : True"라고 출력됨
+
+            int x = 10;
+            string str6 = str5 + "  " + ":" + x;
+            Console.WriteLine("str6 : {0}", str6);
+            // 콘솔에 "str6 : zzz  :10"라고 출력됨
+
+            string str7 = "HelloWorld!";
+            string str8 = str7.Replace("Hello", "Hi"); // "HelloWorld" 에서 "Hello"부분을 "Hi"로 바꿈
+            Console.WriteLine("str8 : {0}", str8);
+            // 콘솔에 "str8 : HiWorld!"라고 출력됨
+
+            Console.WriteLine("str8.ToUpper() : {0}", str8.ToUpper()); // 콘솔에 "str8.ToUpper() : HIWORLD!"라고 출력됨
+            Console.WriteLine("str8.ToLower() : {0}", str8.ToLower()); // 콘솔에 "str8.ToLower() : hiworld!"라고 출력됨
+            // string.ToUpper : 문자열에 포함된 문자를 모두 대문자로 바꿈.
+            // string.ToLower : 문자열에 포함된 문자를 모두 소문자로 바꿈.
+        }
+    }
+}
