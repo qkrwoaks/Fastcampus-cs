f639d33103902accdd71f705f7280d516f7ee0d5
qkrwoaks <82390527+qkrwoaks@users.noreply.github.com>
2022-06-08 09:04:09 +0900

    Ch 03. 데이터란 무엇일까 - 07. 기본 데이터3 - 2
    -솔루션 028_Data_reference 추가

---
+++ c#/Ch 03/FastCampus_Sample_CS/028_Data_reference/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*---------------------------------------------------------------------------
+ * Name : _028_Data_Reference
+ * DESC : Reference참조 기초(두 개의 값이 서로 영향을 준다)
+--------------------------------------------------------------------------- */
+
+namespace _028_Data_Reference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 100;
+            int refNum = num;
+            refNum = 1000;
+
+            Console.WriteLine("refStr : {0}     ReferenceEquals : {1}", num, Object.ReferenceEquals(num, refNum)); // 콘솔에 "refStr: 100     ReferenceEquals: False"라고 출력됨
+
+            int[] arrNum = { 100, 200 };
+            int[] refArrNum = arrNum;
+            refArrNum[0] = 1000;
+
+            Console.WriteLine("refStr : {0}     ReferenceEquals : {1}", num, Object.ReferenceEquals(arrNum, refArrNum)); // 콘솔에 "refStr : 100     ReferenceEquals: True"라고 출력됨
+        }
+    }
+}
+
+// 참조형(reference)
+// 배열, string, class, interface, delegate
+// 두개의 변수가 서로 영향을 줌 (스택 영역 변수, 힙 영역 변수)
+// 값을 저장하는 것이 아닌 주소를 참조
---
+++ c#/Ch 03/FastCampus_Sample_CS/029_Data_boxing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*---------------------------------------------------------------------------
+ * Name : _029_Data_Reference
+ * DESC : boxing, unboxing기초(boxing하는 과정에서 heap 영역에 메모리가 할당)
+--------------------------------------------------------------------------- */
+
+namespace _029_Data_boxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 123;
+            object o = i; // boxing
+            int j = (int)o; // unboxing
+            i = 456;
+
+            Console.WriteLine("i : {0}, o : {1}, j : {2}", i, o, j); // 콘솔에 "i: 456, o: 123, j: 123"라고 출력됨
+            Console.WriteLine("ReferenceEquals : {0}", Object.ReferenceEquals(i, o)); // 콘솔에 "ReferenceEquals: False"라고 출력됨
+        }
+    }
+}
