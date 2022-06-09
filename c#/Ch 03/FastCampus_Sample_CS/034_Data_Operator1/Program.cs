1fa58e58a468547dd6b66dcf5e86e8d0f62e5ffa
qkrwoaks <82390527+qkrwoaks@users.noreply.github.com>
2022-06-09 20:13:55 +0900

    Ch 04. 기초문법 - 02. 프로그램 제어하기 1 - 1
    -프로젝트 FastCampus_Sample_Cs , 솔루션 034_Operator1, 035_Operator2 생성

---
+++ c#/Ch 03/FastCampus_Sample_CS/034_Data_Operator1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*---------------------------------------------------------------------------
+ * Name : _033_Check
+ * DESC : 산술 연산자, +, -, *, /, % (모든 수치 데이터 형태)
+--------------------------------------------------------------------------- */
+
+namespace _034_Data_Operator1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            Console.WriteLine("c : " + c); // 콘솔에 "c: 24"라고 출력됨
+
+            Console.WriteLine("100 - 10 = " + (100 - 10)); // 콘솔에 "100 - 10 = 90"라고 출력됨
+
+            int d = a / b;
+            Console.WriteLine("d : " + d); // 콘솔에 "d: 3"라고 출력됨
+
+            int e = a * (b + c);
+            Console.WriteLine("e : " + e); // 콘솔에 "e: 540"라고 출력됨
+
+            int f = (a + b) % 2;
+            Console.WriteLine("f : " + f); // 콘솔에 "f: 0"라고 출력됨
+        }
+    }
+}
---
+++ c#/Ch 04/FastCampus_Sample_CS/035_Operator2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*---------------------------------------------------------------------------
+ * Name : _035_Operator2
+ * DESC : 산술 연산자, +, -, *, /, % (모든 수치 데이터 형태)
+--------------------------------------------------------------------------- */
+
+namespace _035_Operator2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 3;
+            float c = 123.45f, d = 3.1456f;
+
+            Console.WriteLine("a : " + a); // 콘솔에 "a: 10"라고 출력됨
+            Console.WriteLine("b : " + b); // 콘솔에 "b: 3"라고 출력됨
+            Console.WriteLine("c : " + c); // 콘솔에 "c: 123.45"라고 출력됨
+            Console.WriteLine("d : " + d); // 콘솔에 "d: 3.1456"라고 출력됨
+
+            Console.WriteLine("\n정수의 계산");
+            Console.WriteLine("a + b = " + (a + b)); // 콘솔에 "a + b = 13"라고 출력됨
+            Console.WriteLine("a - b = " + (a - b)); // 콘솔에 "a - b = 7"라고 출력됨
+            Console.WriteLine("a * b = " + (a * b)); // 콘솔에 "a * b = 30"라고 출력됨
+            Console.WriteLine("a / b = " + (a / b)); // 콘솔에 "a / b = 3"라고 출력됨
+            Console.WriteLine("a % b = " + (a % b)); // 콘솔에 "a % b = 1"라고 출력됨
+        }
+    }
+}
