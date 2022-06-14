b4b53288b120f62ed7020fdff52b08dbfec6fe36
qkrwoaks <82390527+qkrwoaks@users.noreply.github.com>
2022-06-14 23:35:56 +0900

    Ch 04. 기초문법 - 02. 프로그램 제어하기 1 - 2 (미완)

---
+++ c#/Ch 04/FastCampus_Sample_CS/036_Operator3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*---------------------------------------------------------------------------
+ * Name : _036_Operator3
+ * DESC : '++', '--' 증감 연산자에 대해 알아봄
+--------------------------------------------------------------------------- */
+
+namespace _036_Operator3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+
+            Console.WriteLine("num : " + num++);
+            Console.WriteLine("num : " + num);
+            Console.WriteLine("num : " + ++num);
+
+            Console.WriteLine("num : " + num--);
+            Console.WriteLine("num : " + num);
+            Console.WriteLine("num : " + --num);
+        }
+    }
+}
