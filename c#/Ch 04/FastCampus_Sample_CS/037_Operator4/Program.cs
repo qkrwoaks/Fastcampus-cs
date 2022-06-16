2e0e5e6969aef9c076b752a593a88a1607b677f9
qkrwoaks <82390527+qkrwoaks@users.noreply.github.com>
2022-06-16 22:52:41 +0900

    Ch 04 기초문법 - 03 프로그램 제어하기 1 - 3

---
+++ c#/Ch 04/FastCampus_Sample_CS/037_Operator4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*---------------------------------------------------------------------------
+ * Name : _037_Operator4
+ * DESC : 관계 연산자, 피연산자의 비교(bool)
+--------------------------------------------------------------------------- */
+
+namespace _037_Operator4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result;
+            int a = 100;
+            int b = 1000;
+
+            result = a == b;
+            Console.WriteLine(result);
+
+            result = a != b;
+            Console.WriteLine(result);
+
+            result = a > b;
+            Console.WriteLine(result);
+
+            result = a < b;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ c#/Ch 04/FastCampus_Sample_CS/038_Operator5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*---------------------------------------------------------------------------
+ * Name : _038_Operator5
+ * DESC : 
+ * 조건부 논리  '&&', '||'
+ * 논리 부정 연산자 '!'
+--------------------------------------------------------------------------- */
+
+namespace _038_Operator5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result;
+            int a = 100;
+            int b = 1000;
+
+            result = a == 100 && b == 1000;
+            Console.WriteLine(result);
+
+            result = a == 100 || b == 1000;
+            Console.WriteLine(result);
+
+            result = a == 100 && b == 100;
+            Console.WriteLine(result);
+
+            result = a == 100 || b == 100;
+            Console.WriteLine(result);
+        }
+    }
+}
