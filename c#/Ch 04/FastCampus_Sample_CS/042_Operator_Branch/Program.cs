a0bcedba0edd6b39b33a4c1bda1675a8e5eee425
qkrwoaks <82390527+qkrwoaks@users.noreply.github.com>
2022-06-23 21:07:09 +0900

    Ch 04. 기초문법 - 06. 프로그램 제어하기2 - 2

---
+++ c#/Ch 04/FastCampus_Sample_CS/042_Operator_Branch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*---------------------------------------------------------------------------
+ * Name : _042_Operator_Branch
+ * DESC : null 병합 연산자
+ * null 병합 연산자 '??'
+ * null 값을 체크하는 연산자 "null 인가 ?? 처리1 => true, false"
+--------------------------------------------------------------------------- */
+
+namespace _042_Operator_Branch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isBool = false;
+
+            if (isBool)
+            {
+                Console.WriteLine("isbool : " + isBool);
+            }
+            else
+            {
+                Console.WriteLine("isbool : " + isBool);
+            }
+        }
+    }
+}
