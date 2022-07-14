7f187072490f84b5f281bc29a145c33b556e0ca5
박재만 <s21009@gsm.hs.kr>
2022-07-14 13:30:16 +0900

    Ch 04. 기초문법 - 06. 프로그램 제어하기2 - 2 -2

---
+++ c#/Ch 04/FastCampus_Sample_CS/047_Operator_While/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _047_Operator_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            int total = 0;
+            int total2 = 0;
+
+            while (a <= 10 || b <= 10)
+            {
+                total += a++;
+                total2 = total + b++;
+            }
+
+            Console.WriteLine(total);
+            Console.WriteLine(total2);
+        }
+    }
+}
