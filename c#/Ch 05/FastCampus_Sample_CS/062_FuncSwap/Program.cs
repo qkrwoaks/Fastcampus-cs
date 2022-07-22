97cfbdd3e435764d2400caa7489a1bd897da9705
qkrwoaks <qkrwoaks5327@naver.com>
2022-07-23 02:06:44 +0900

    Ch 05. 함수 마스터하기- 04. 함수 제대로 활용해보기 - 1

---
+++ c#/Ch 05/FastCampus_Sample_CS/062_FuncSwap/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _062_FuncSwap
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 값을 바꾸는 함수
+        /// </summary>
+        /// <param name="a">첫번째 변수</param>
+        /// <param name="b">두번째 변수</param>
+        static public void ValueSwap(int a, int b)
+        {
+            int temp = a; // 두 변수의 값을 바꿀때 사용할 임시 변수
+            a = b;
+            b = temp;
+
+            Console.WriteLine("ValueSwap");
+            Console.WriteLine("a : {0} , b : {1}", a, b);
+        }
+
+        static public void RefSwap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine("RefSwap");
+            Console.WriteLine("a : {0}, b : {1}", a, b);
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = -100;
+
+            ValueSwap(a, b); // 자료형의 값 변경 (출력시 바뀜)
+            Console.WriteLine("a : {0}, b : {1}", a, b);
+
+            RefSwap(ref a, ref b); // 자료형의 주소 변경 (출력시 바뀌지 않음)
+            Console.WriteLine("a : {0}, b : {1}", a, b);
+        }
+    }
+}
+// Call By Value - 값에 의한 호출 / 함수에서 값에 영향을 주지 않는다 / 일반함수
+// Call By Reference - 주소에 의한 호출 / 함수에서 값에 영향을 준다 / ref 키워드를 가지는 함수
--- c#/Ch 05/FastCampus_Sample_CS/60_Func3/Program.cs
+++ c#/Ch 05/FastCampus_Sample_CS/60_Func3/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _60_Func3
+namespace _060_Func3
 {
     internal class Program
     {
