fbb2bec97ff1f01fbd33a084b3b8cc64f4f520c5
qkrwoaks <qkrwoaks5327@naver.com>
2022-07-22 16:41:11 +0900

    Ch 05. 함수 마스터하기 - 02. 함수 처음 배워보기 - 2

---
+++ c#/Ch 05/FastCampus_Sample_CS/059_Func2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _059_Func2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            num += Add(); // 1
+
+            Console.WriteLine("Num : " + num);
+            Console.WriteLine("입력하신 정수는 {0} 입니다.", InputNum());
+        }
+
+        static int Add() 
+        {
+            int a = 0;
+            return ++a;
+        }
+
+        static int InputNum()
+        {
+            Console.Write("정수를 입력하세요 : ");
+            int num = int.Parse(Console.ReadLine());
+            return num;
+        }
+
+        
+    }
+}
---
+++ c#/Ch 05/FastCampus_Sample_CS/60_Func3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _60_Func3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int resultA = Square(a);
+            Console.WriteLine("resultA : " + resultA);
+
+            int b = 4;
+            int resultB = Square(b);
+            Console.WriteLine("resultB : " + resultB);
+
+            int resultC = Square(6);
+            Console.WriteLine("resultC : " + resultC);
+            
+            int resultD = Square(resultA * 3);
+            Console.WriteLine("resultD : " + resultD);
+        }
+
+        static int Square(int i)
+        {
+            int input = i;
+            return input * input;
+        }
+    }
+}
