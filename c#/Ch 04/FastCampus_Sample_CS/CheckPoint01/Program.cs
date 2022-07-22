0e4cf1af0bad658a24e37274230026a75ec2bee2
qkrwoaks <qkrwoaks5327@naver.com>
2022-07-22 16:19:59 +0900

    Ch 04. 기초문법 - 09. CheckPoint01 - 게임 만들기 (숫자달리기) - 2

--- c#/Ch 04/FastCampus_Sample_CS/CheckPoint01/Program.cs
+++ c#/Ch 04/FastCampus_Sample_CS/CheckPoint01/Program.cs
@@ -12,8 +12,9 @@ namespace CheckPoint01
         static void Main(string[] args)
         {
             Random random = new Random();
-            const string LINE = "----------------------------------------";
+            const string LINE = "---------------------------------------------";
             const int END_Line = 42;
+            const int DELAY_TIME = 100;
 
             int runA = 0;
             int runB = 0;
@@ -22,7 +23,7 @@ namespace CheckPoint01
 
             while (true)
             {
-                Thread.Sleep(1000); // 지정된 시간 만큼 스레드를 일시 중단 하는 함수 (딜레이)
+                Thread.Sleep(DELAY_TIME); // 지정된 시간 만큼 스레드를 일시 중단 하는 함수 (딜레이)
                 Console.Clear(); // 콘솔 버퍼, 창에 표시 정보를 지우는 함수 (화면 지우기)
 
                 runA++;
@@ -31,41 +32,88 @@ namespace CheckPoint01
                 runD++;
 
                 int randomNumber = random.Next(0, 4); // 0 ~ 3
+                int runRandomNumber = random.Next(0, 2); // 0 ~ 1
 
                 switch (randomNumber)
                 {
                     case 0:
-                        ++runA;
+                        runA += runRandomNumber;
                         break;
                     case 1:
-                        ++runB;
+                        runB += runRandomNumber;
                         break;
                     case 2:
-                        ++runC;
+                        runC += runRandomNumber;
                         break;
                     case 3:
-                        ++runD;
+                        runD += runRandomNumber;
                         break;
                 }
 
                 Console.WriteLine(LINE);
                 for (int i = 0; i < runA; i++)
                     Console.Write(" ");
-                Console.WriteLine("1");
+                Console.Write("1");
 
-                for (int i = 0; i < runA; i++)
+                for (int i = (END_Line) - runA; i >= 0; i--)
                     Console.Write(" ");
-                Console.WriteLine("2");
+                Console.WriteLine("|");
 
-                for (int i = 0; i < runA; i++)
+                for (int i = 0; i < runB; i++)
                     Console.Write(" ");
-                Console.WriteLine("3");
+                Console.Write("2");
 
-                for (int i = 0; i < runA; i++)
+                for (int i = (END_Line) - runB; i >= 0; i--)
                     Console.Write(" ");
-                Console.WriteLine("4");
+                Console.WriteLine("|");
+
+                for (int i = 0; i < runC; i++)
+                    Console.Write(" ");
+                Console.Write("3");
+
+                for (int i = (END_Line) - runC; i >= 0; i--)
+                    Console.Write(" ");
+                Console.WriteLine("|");
+
+                for (int i = 0; i < runD; i++)
+                    Console.Write(" ");
+                Console.Write("4");
+
+                for (int i = (END_Line) - runD; i >= 0; i--)
+                    Console.Write(" ");
+                Console.WriteLine("|");
 
                 Console.WriteLine(LINE);
+
+                if (runA >= END_Line || runB >= END_Line || runC >= END_Line || runD >= END_Line)
+                {
+                    int runNum = 0;
+                    string stringResult = "결과 : !! {0} 선수 우승 !!";
+
+                    if (runA >= END_Line)
+                        runNum = 1;
+                    else if (runB >= END_Line)
+                        runNum = 2;
+                    else if (runC >= END_Line)
+                        runNum = 3;
+                    else if (runD >= END_Line)
+                        runNum = 4;
+
+                    Console.WriteLine(stringResult, runNum);
+
+                    Console.Write("다시하려면 0번 입력 : ");
+                    if (Console.ReadLine() == "0")
+                    {
+                        runA = 0;
+                        runB = 0;
+                        runC = 0;
+                        runD = 0;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
             }
         }
     }
