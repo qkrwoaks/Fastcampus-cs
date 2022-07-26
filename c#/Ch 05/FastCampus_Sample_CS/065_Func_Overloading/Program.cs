ffc760f23cc5b36376cb1baf3ec6e6e5305b8011
qkrwoaks <qkrwoaks5327@naver.com>
2022-07-27 00:00:02 +0900

    Ch 05. 함수 마스터하기 - 05. 함수 제대로 활용해보기 - 2

---
+++ c#/Ch 05/FastCampus_Sample_CS/065_Func_Overloading/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _065_Func_Overloading
+{
+    internal class Program
+    {
+        static double Add(int a, int b)
+        {
+            return a + b;
+        }
+        static double Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        // Add(int a, int b, int c) 모호성으로 오류 발생
+        static int Add(int a, int b, int c, int d = 0)
+        {
+            return (a + b + c + d) * 2;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add(int a, int b) : " + Add(10, 100));
+            Console.WriteLine("Add(int a, int b, int c) : " + Add(10, 100, 1000));
+            Console.WriteLine("Add(double a, double b) : " + Add(10.3, 100.5));
+        }
+    }
+}
