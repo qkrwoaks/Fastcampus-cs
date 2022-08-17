6640a72457b3e048c6e02602f3cdb2daba993ab1
qkrwoaks <qkrwoaks5327@naver.com>
2022-08-17 21:48:43 +0900

    Ch 07. 클래스 개념 꽉 잡기 - 05. 상속과 도형 - 클래스 상속

---
+++ c#/Ch 07/FastCampus_Sample_CS/081_01_class_Inheritance/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _081_01_class_Inheritance
+{
+    class Super 
+    {
+        protected int a;
+
+        public void Print()
+        {
+            Console.WriteLine("Super Print()");
+        }
+    }
+
+    class Sub : Super
+    {
+        int b;
+
+        public void Print()
+        {
+            Console.WriteLine("Sub Print() a : {0}, b : {1}", a, b);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            Super super = new Super();
+            Sub sub = new Sub();
+            sub.Print();
+        }
+    }
+}
---
+++ c#/Ch 07/FastCampus_Sample_CS/081_02_classInheritance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _081_02_classInheritance
+{
+    class Sub
+    {
+        protected int a;
+
+        public Sub()
+        {
+            a = 100;
+            Console.WriteLine(" Call Sub 생성자 호출");
+        }
+
+        ~Sub()
+        {
+            Console.WriteLine(" Call Sub 소멸자 호출");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sub sub = new Sub();
+        }
+    }
+}
