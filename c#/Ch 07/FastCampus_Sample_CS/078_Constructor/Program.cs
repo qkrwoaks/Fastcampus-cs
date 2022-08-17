41326284a84c24b5b9bd8d5d97d3b5edc3016b91
qkrwoaks <qkrwoaks5327@naver.com>
2022-08-17 20:46:43 +0900

    Ch 07. 클래스 개념 꽉 잡기 - 03. 쉽게 이해하는 클래스 문법 - new 키워드, 생성자, 소멸자

---
+++ c#/Ch 07/FastCampus_Sample_CS/078_Constructor/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _078_Constructor
+{
+    class AA
+    {
+        int a;
+        float f;
+
+        /// <summary>
+        /// 생성자
+        /// </summary>
+        public AA()
+        {
+            a = 0;
+            f = 0f;
+            Console.WriteLine(" Call Constructor ");
+            Console.WriteLine(" a : {0}", a);
+            Console.WriteLine(" f : {0}", f);
+        }
+
+        public AA(int _a)
+        {
+            a = _a;
+            Console.WriteLine(" Call Constructor");
+            Console.WriteLine(" a : {0}", a);
+            Console.WriteLine(" f : {0}", f);
+        }
+
+        public AA(int _a, float _f)
+        {
+            a = _a;
+            f = _f;
+
+            Console.WriteLine(" Call Constructor ");
+            Console.WriteLine(" a : {0}", a);
+            Console.WriteLine(" f : {0}", f);
+        }
+
+        /// <summary>
+        /// 소멸자
+        /// </summary>
+        ~AA()
+        {
+            Console.WriteLine(" Call Destructor");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AA aa = new AA();
+            AA aaa = new AA(10);
+            AA aaaa = new AA(100, 0.5f);
+        }
+    }
+}
