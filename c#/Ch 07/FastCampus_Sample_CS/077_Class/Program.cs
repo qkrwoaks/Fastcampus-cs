6a71840b5589efd7a215e58f667d711892c089b3
qkrwoaks <qkrwoaks5327@naver.com>
2022-08-17 20:31:08 +0900

    Ch 07. 클래스 개념 꽉 잡기 - 02. 쉽게 이해하는 클래스 문법 - 클래스 개념

---
+++ c#/Ch 07/FastCampus_Sample_CS/077_Class/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _077_Class
+{
+    class AA
+    {
+        int num1;
+        public int num2, num3;
+
+        public void Print()
+        {
+            Console.WriteLine("aa : {0}, bb : {1}, cc : {2}", num1, num2, num3);
+        }
+
+        private void PrintPrivate()
+        {
+            Console.WriteLine("aa : {0}, bb : {1}, cc : {2}", num1, num2, num3);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AA aa = new AA();
+            aa.num2 = 100;
+            aa.num3 = 1000;
+
+            aa.Print();
+            // aa.PrintPrivate(); // 오류
+        }
+    }
+}
