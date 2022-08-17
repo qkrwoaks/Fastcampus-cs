be44537221b93c1f5754449d5ab590338024bebd
qkrwoaks <qkrwoaks5327@naver.com>
2022-08-17 23:42:55 +0900

    Ch 07. 클래스 개념 꽉 잡기 - 07. 상속과 도형 - 다형성

---
+++ c#/Ch 07/FastCampus_Sample_CS/083_override/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _083_override
+{
+    class Super
+    {
+        protected int num;
+
+        public virtual void Print()
+        {
+            Console.WriteLine("num : {0}", num);
+        }
+    }
+
+    class AA : Super
+    {
+        public int a;
+
+        public override void Print()
+        {
+            base.Print();
+
+            Console.WriteLine("AA a : {0}", a);
+        }
+    }
+
+    class BB : Super
+    {
+        public int b;
+
+        public override void Print()
+        {
+            base.Print();
+
+            Console.WriteLine("BB b : {0}", b);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Super super = new Super();
+            super.Print();
+
+            Super aa = new AA();
+            aa.Print();
+
+            Super bb = new BB();
+            bb.Print();
+        }
+    }
+}
