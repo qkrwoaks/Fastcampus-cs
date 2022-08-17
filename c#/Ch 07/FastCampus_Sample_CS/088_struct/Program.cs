31043892b1dbe34de7666716dbb19bd05d6a5018
qkrwoaks <qkrwoaks5327@naver.com>
2022-08-18 01:17:23 +0900

    Ch 07. 클래스 개념 꽉 잡기 - 10. 인터페이스와 추상 클래스 - 인터페이스

--- c#/Ch 07/FastCampus_Sample_CS/088_struct/Program.cs
+++ c#/Ch 07/FastCampus_Sample_CS/088_struct/Program.cs
@@ -5,11 +5,44 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace _088_struct
-{
+{   
+    struct AA
+    {
+        public int x;
+        public int y;
+
+        // public AA() { } // 오류 : 파라미터 없음
+        public AA(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("x : {0}, y : {1}", x, y);
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            AA aa = new AA(10, 20);
+            aa.x = 100;
+            aa.Print();
+
+            AA aaa;
+            aaa.x = 100;
+            aaa.y = 200;
+            aaa.Print();
+
+            AA copyAA = aa;
+            copyAA.x = 1000;
+            copyAA.y = 2000;
+            aa.Print();
+
+            // copyAA.Print();
         }
     }
 }
