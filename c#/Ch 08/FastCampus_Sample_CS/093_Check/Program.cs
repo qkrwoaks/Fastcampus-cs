69757b601af0dbb651f2980e90cb8d3401c75fff
qkrwoaks <qkrwoaks5327@naver.com>
2022-08-18 19:24:29 +0900

    Ch 08. 컬렉션, 인덱서 - 01. 컬렉션, 인덱서 - 1

---
+++ c#/Ch 08/FastCampus_Sample_CS/093_Check/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _093_Check
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ c#/Ch 08/FastCampus_Sample_CS/095_ArrayList/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _095_ArrayList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrList = new ArrayList();
+
+            arrList.Add("Hello");
+            arrList.Add(10f);
+
+            for (int i = 0; i < 10; i++)
+            {
+                arrList.Add(i);
+            }
+
+            foreach (object data in arrList)
+            {
+                Console.WriteLine("arrList data : " + data);
+            }
+
+            Console.WriteLine("배열데이터로 초기화");
+            int[] arrData = { 100, 200, 300 };
+            ArrayList arrCopyList = new ArrayList(arrData);
+
+            foreach (object data in arrCopyList)
+            {
+                Console.WriteLine("arrCopyList data : " + data);
+            }
+        }
+    }
+}
