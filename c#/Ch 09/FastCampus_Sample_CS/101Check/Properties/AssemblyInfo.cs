8d73f5f7d7a35b49bc5082c8152d26806e392702
박재만 <s21009@gsm.hs.kr>
2022-08-31 11:06:17 +0900

    Ch 09. C# 초보탈출 I - 01. 일반화 프로그래밍 - 1

--- c#/Ch 09/FastCampus_Sample_CS/101Check/Properties/AssemblyInfo.cs
+++ c#/Ch 09/FastCampus_Sample_CS/101Check/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("099_Index01")]
+[assembly: AssemblyTitle("101Check")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("099_Index01")]
+[assembly: AssemblyProduct("101Check")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("f8b50d07-6246-4d79-9cf6-735e6dc98fe0")]
+[assembly: Guid("e7edc997-6ee2-4e1d-9655-d19bdd3b99b4")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
---
+++ c#/Ch 09/FastCampus_Sample_CS/103_GenericFunc/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _103_GenericFunc
+{
+    internal class Program
+    {
+        static void GenericPrint<T>(T[] arrData)
+        {
+            for (int i = 0; i < arrData.Length; i++)
+            {
+                Console.WriteLine("arrData : {0}" + arrData[i]);
+            }
+        }
+
+        static void GenericPrint<T>(T arrData)
+        {
+            Console.WriteLine("arrData : " + arrData);
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 10;
+            float b = 10.3f;
+            string c = "Hello";
+
+            int[] arrA = { 0, 1, 2, 3 };
+            string[] arrB = {"A", "B", "C", "D" };
+
+            GenericPrint(a);
+            GenericPrint(b);
+            GenericPrint(c);
+
+            Console.WriteLine();
+            GenericPrint(arrA);
+            Console.WriteLine();
+            GenericPrint(arrB);
+        }
+    }
+}
