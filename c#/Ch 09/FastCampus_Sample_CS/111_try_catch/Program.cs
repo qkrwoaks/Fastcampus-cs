387af52c7782f02840bb4ec1b531d0966010f26d
박재만 <s21009@gsm.hs.kr>
2022-08-31 20:57:10 +0900

    Ch 09. C# 초보탈출 I - 03. 예외 처리 - 1

---
+++ c#/Ch 09/FastCampus_Sample_CS/111_try_catch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _111_try_catch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNum = 0;
+            bool isCorrct = false;
+
+            while (!isCorrct)
+            {
+                string readStr = Console.ReadLine();
+
+                try
+                {
+                    inputNum = int.Parse(readStr);
+                    isCorrct = true;
+                }
+
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("입력문자 : " + readStr + "정수 입력하세요");
+                }
+            }
+
+            Console.WriteLine("inputNum : " + inputNum);
+        }
+    }
+}
