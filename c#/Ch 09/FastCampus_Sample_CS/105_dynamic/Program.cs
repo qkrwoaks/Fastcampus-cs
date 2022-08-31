e4469f3c57ded48bb474ece7d36f3474e7fe900b
박재만 <s21009@gsm.hs.kr>
2022-08-31 19:08:02 +0900

    Ch 09. C# 초보탈출 I - 02. 일반화 프로그래밍 - 2

---
+++ c#/Ch 09/FastCampus_Sample_CS/105_dynamic/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _105_dynamic
+{
+    internal class Program
+    {
+        static T AddArray<T>(T[] arrDatas)
+        {
+            // T temp = 0; // 에러 발생
+            // object aaa = 0; // 박싱, 언박싱 발생
+            dynamic temp = default(T);
+            for (int i = 0; i < arrDatas.Length; i++)
+            {
+                temp += arrDatas[i];
+            }
+            return temp;
+        }
+
+        static T SumArray<T>(T[] arrDatas)
+        {
+            T temp = default(T);
+            for (int i = 0; i < arrDatas.Length; i++)
+            {
+                temp += (dynamic)arrDatas[i];
+            }
+            return temp;
+        }
+
+        static void PrintArray<T>(T[] arrDatas)
+        {
+            foreach(var data in arrDatas)
+            {
+                Console.WriteLine("data : {0}", data);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arrNums = { 1, 2, 3, 4, 5 };
+
+            var vv = "vv"; // vv. -> var. 에 해당하는 모든 값
+            object oo = "oo"; // oo. -> 해당 변수에 해당하는 모든 값
+            dynamic dd = "dd"; // dd. -> 없음(런타임에서 값이 결정되기 때문)
+
+            Console.WriteLine("AddArray : {0}", AddArray(arrNums));
+            PrintArray(arrNums);
+        }
+    }
+}
---
+++ c#/Ch 09/FastCampus_Sample_CS/107_List/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _107_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listAA = new List<int>();
+            listAA.Add(1);
+            listAA.Add(2);
+
+            for (int i = 0; i < 10; i++)
+            {
+                listAA.Add(i);
+            }
+
+            foreach (var data in listAA)
+            {
+                Console.WriteLine("data : " + data);
+            }
+
+            Console.WriteLine("\n배열데이터로 초기화");
+            string[] arrData = { "AA", "BB", "CC" };
+            List<string> listArr = new List<string>(arrData);
+
+            for (int i = 0; i < arrData.Length; i++)
+            {
+                Console.WriteLine("arrData : " + arrData[i]);
+            }
+        }
+    }
+}
