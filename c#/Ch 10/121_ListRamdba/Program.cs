4ad805f8efb58d8335caba6090c631d5bfb30684
qkrwoaks <qkrwoaks5327@naver.com>
2022-09-03 23:55:33 +0900

    Ch 10. C# 초보탈출 II - 01. 람다식

---
+++ c#/Ch 10/121_ListRamdba/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _121_ListRamdba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listData = new List<int> { 1, 2, 3, 100, 200, 300 };
+
+            // public delegate bool Predicate<in T>(T obj);
+            // public List<T> FindAll(Predicate<T> match);
+
+            List<int> listFindALL = listData.FindAll((num) => { return num < 200; }); // (num) => num < 100
+
+            Console.WriteLine("200보다 작은 모든 수 : ");
+            foreach (int a in listFindALL)
+            {
+                Console.WriteLine("a : " + a);
+            }
+            
+            int findNum = listData.Find((num) => num % 2 == 0);
+            Console.WriteLine("첫번째 짝수 : " + findNum);
+        }
+    }
+}
---
+++ c#/Ch 10/122_FuncRamdba/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _122_FuncRamdba
+{
+    delegate void dPrint(string str);
+    delegate void dFunc();
+
+    internal class Program
+    {
+        static public void CallPrint(string str)
+        {
+            Console.WriteLine(str);
+        }
+
+        static public void CallBackFunc(dPrint dp, string msg)
+        {
+            if (null != dp)
+            {
+                dp("CallBackFunc : " + msg);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CallBackFunc(CallPrint, "Hello"); // 함수 연결
+            CallBackFunc(delegate (string str) { Console.WriteLine(str); }, "Hello"); // delegate 직접    
+            CallBackFunc((string str) => { Console.WriteLine(str); }, "Hello"); // 람다의 식형별
+            CallBackFunc((str) => Console.WriteLine(str), "Hello"); // 람다식 기본..
+            CallBackFunc(str => Console.WriteLine(str), "Hello"); // 람다식 기본..
+
+            dFunc dfunc = () => Console.WriteLine("No Params"); // 파라미터가 없는 경우 () 반드시 사용 
+        }
+    }
+}
