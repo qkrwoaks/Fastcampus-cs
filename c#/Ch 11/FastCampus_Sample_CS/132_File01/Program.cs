cff9955a1a3297150be4c3351304a7bdd3cdb271
박재만 <s21009@gsm.hs.kr>
2022-09-05 13:34:10 +0900

    Ch 11. C# 초보탈출 III - 01. 파일 처리 - 1

---
+++ c#/Ch 11/FastCampus_Sample_CS/132_File01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _132_File01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "";
+
+            if (args.Length <= 0)
+            {
+                path = Directory.GetCurrentDirectory();
+                path += "\\a.txt";
+
+                Console.WriteLine("path : " + path);
+            }
+            else
+            {
+                path = args[0];
+            }
+
+            if (File.Exists(path)) // using System.IO;
+            {
+                Console.WriteLine("\nGetCreationTime : " + File.GetCreationTime(path));
+            }
+            else
+            {
+                FileStream fs = File.Create(path);
+                fs.Close();
+            }
+
+            FileInfo fileInfo = new FileInfo("b.txt");
+            FileStream ff = fileInfo.Create();
+
+            ff.Close();
+        }
+    }
+}
---
+++ c#/Ch 11/FastCampus_Sample_CS/133_File_BitConverter/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace _133_File_BitConverter
+{
+    internal class Program
+    {
+        const string fileName = "a.txt";
+
+        static void Main(string[] args)
+        {
+            // 파일 쓰기
+            long lValue = 1234567890123456789;
+            int num = 100;
+            Console.WriteLine("lValue : " + lValue);
+            Console.WriteLine("num : " + num);
+
+            Stream outStream = new FileStream(fileName, FileMode.Create);
+            byte[] wByte = BitConverter.GetBytes(lValue);
+
+            Console.Write("Byte : ");
+
+            foreach (var item in wByte) Console.Write("{0:X2} ", item);
+            Console.WriteLine();
+            
+            outStream.Write(wByte, 0, wByte.Length);
+
+            wByte = BitConverter.GetBytes(num); // sizeof(int)
+
+            Console.Write("Byte : ");
+
+            foreach (var item in wByte) Console.Write("{0:X2} ", item);
+            Console.WriteLine();
+
+            outStream.Write(wByte, 0, wByte.Length);
+
+            outStream.Close();
+
+            // 파일 읽기
+            FileStream inStream = new FileStream(fileName, FileMode.Open);  
+
+            // rByte의 길이만큼 데이터를 읽어 rBytes에 저장
+            byte[] rbytes = new byte[sizeof(long)];
+            inStream.Read(rbytes, 0, rbytes.Length);
+            long readValue = BitConverter.ToInt64(rbytes, 0);
+
+            rbytes = new byte[sizeof(int)];
+            inStream.Read(rbytes, 0, rbytes.Length);
+            int readNum = BitConverter.ToInt32(rbytes, 0);
+
+            Console.WriteLine("Read Data : " + readValue);
+            Console.WriteLine("Read Data : " + readNum);
+
+            inStream.Close();
+        }
+    }
+}
