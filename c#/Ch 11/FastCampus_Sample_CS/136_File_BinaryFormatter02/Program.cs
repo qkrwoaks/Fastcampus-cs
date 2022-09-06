bd5626bd44aade3421fb293606f4b63ab6bc8a19
박재만 <s21009@gsm.hs.kr>
2022-09-06 20:41:05 +0900

    Ch 11. C# 초보탈출 III - 02. 파일 처리 - 2

---
+++ c#/Ch 11/FastCampus_Sample_CS/136_File_BinaryFormatter02/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace _136_File_BinaryFormatter02
+{
+    [Serializable]
+    struct Player
+    {
+        public string _Name;
+        public int _Level;
+        public double _Exp;
+    }
+    internal class Program
+    {
+        const string fileName = "list.dat";
+        static void Main(string[] args)
+        {
+            List<Player> listPlayers = new List<Player>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Player player = new Player();
+                player._Name = i.ToString();
+                player._Level = i;
+                player._Exp = i * 10;
+
+                listPlayers.Add(player);
+            }
+
+            // 쓰기
+            FileStream fsW = new FileStream(fileName, FileMode.Create);
+
+            BinaryFormatter bfW = new BinaryFormatter();
+            bfW.Serialize(fsW, listPlayers);
+
+            fsW.Close();
+
+            // 읽기
+            FileStream fsr = new FileStream(fileName, FileMode.Open);
+
+            BinaryFormatter bfr = new BinaryFormatter();
+            List<Player> readPlayers = (List<Player>)bfr.Deserialize(fsW);
+
+            foreach (var data in readPlayers)
+            {
+                Console.WriteLine("_Name : {0} _Level : {1} _Exp : {2}", data._Name, data._Level, data._Exp);
+            }
+
+            fsr.Close();
+        }
+    }
+}
---
+++ c#/Ch 11/FastCampus_Sample_CS/138_File_CSV/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _138_File_CSV
+{
+    class Stage
+    {
+        public string stage { get; set; }
+        public int min { get; set; }
+        public int max { get; set; }
+        public int finish { get; set; }
+        public int gold { get; set; }
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string fileName = "test.xlsx";
+
+            int index = 0;
+            List<Stage> listStage = new List<Stage>();
+            List<string> readData = new List<string>();
+
+            using (StreamReader sr = new StreamReader(new FileStream(fileName, FileMode.Open)))
+            {
+                //while(false == sr.EndOfStream)
+                //{
+                //    Console.WriteLine(sr.ReadLine());
+                //}
+
+                while (false == sr.EndOfStream)
+                {
+                    string readStr = sr.ReadLine();
+
+                    if (index++ >= 1)
+                    {
+                        string[] splitData = readStr.Split(',');
+
+                        Stage temp = new Stage();
+                        temp.stage = splitData[0];
+                        temp.min = Convert.ToInt32(splitData[1]);
+                        temp.max = Convert.ToInt32(splitData[2]);
+                        temp.finish = Convert.ToInt32(splitData[3]);
+                        temp.gold = Convert.ToInt32(splitData[4]);
+                        listStage.Add(temp);
+                    }
+                }
+            }
+
+            foreach (var d in listStage)
+            {
+                Console.Write("{0} {1} {2} {3}", d.stage, d.min, d.max, d.finish, d.gold);
+                Console.WriteLine();
+            }
+
+            string str = "0, 1, 2, 3, 4, 5";
+            string[] splitRead = str.Split(',');
+            for (int i = 0; i < splitRead.Length; i++)
+            {
+                Console.Write("  {0}  ", splitRead[i]);
+            }
+        }
+    }
+}
