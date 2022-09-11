c691d18722386b131923646c93066a4c231459d9
qkrwoaks <qkrwoaks5327@naver.com>
2022-09-11 16:22:32 +0900

    Ch 01. 선형자료구조 - 01. 배열

---
+++ data_structure/ConsoleApp1.Program/ConsoleApp1.Program/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lotto test = new Lotto();   
+        }
+
+        class Lotto
+        {
+            private int[] number;
+
+            public Lotto()
+            {
+                GetNumber();
+            }
+
+            private void GetNumber()
+            {
+                Random random = new Random();
+                number = new int[6];
+
+                int index = 0;
+
+                while (index < number.Length)
+                {
+                    int temp = random.Next(1, 46);
+
+                    if (IsSameNumber(index, temp))
+                    {
+                        continue;
+                    }
+
+                    number[index++] = temp;
+                }
+
+                for (int i = 0; i < number.Length; i++)
+                {
+                    Console.WriteLine(number[i]);
+                }
+            }
+
+            private bool IsSameNumber(int index, int temp)
+            {
+                for (int i = 0; i < index; i++)
+                {
+                    if (number[i] == temp) return true;
+                }
+                return false;
+            }
+        }
+    }
+}
