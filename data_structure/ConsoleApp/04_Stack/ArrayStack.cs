5f462a1b19017e4a3e9e5a6668668555c3df6b37
박재만 <82390527+qkrwoaks@users.noreply.github.com>
2022-09-23 13:45:57 +0900

    Ch 01. 선형자료구조 - 06. 스택

---
+++ data_structure/ConsoleApp/04_Stack/ArrayStack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Stack
+{
+    internal class ArrayStack
+    {
+        private int[] m_space; // 전체 공간
+        private int m_top;     // 최상단 가르키고 있는 Top
+        public int Count 
+        { get { return m_space.Length; } }          // 데이터가 몇개 인지
+
+        /// <summary>
+        /// 생성자
+        /// </summary>
+        /// <param name="size"></param>
+        public ArrayStack(int size)
+        {
+            m_space = new int[size];
+            m_top = -1;
+        }
+
+        public void Push(int value)
+        {
+            m_top++;
+            m_space[m_top] = value;
+        }
+
+        public int Pop()
+        {
+            int value = m_space[m_top];
+
+            m_space[m_top] = 0;
+
+            m_top--;
+
+            return value;
+        }
+
+        public bool IsEmpty()
+        {
+            return Count != 0;
+        }
+
+        public bool IsFull()
+        {
+            return m_top == m_space.Length - 1;
+        }
+    }
+}
---
+++ data_structure/ConsoleApp/04_Stack/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1); // 1
+            stack.Push(2); // 1, 2
+            stack.Pop(); // 1
+            stack.Push(3); // 1, 3
+            stack.Push(4); // 1, 3, 4
+            stack.Peek(); // 1, 3, 4
+            stack.Push(5); // 1, 3, 4, 5
+            
+            Console.WriteLine("스택의 갯수 : " + stack.Count);
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+
+            // 제네릭 타입이 아닌 Stack
+            // Stack stack1 = new Stack();
+            // stack1.Push("문자열");
+            // stack1.Push(1);
+            // stack1.Push(true);
+
+            // while (stack1.Count > 0)
+            //      Console.WriteLine(stack1 Pop());
+
+            // stack1.Push("ArrayStack");
+
+
+
+            //ArrayStack arrayStack = new ArrayStack(5);
+
+            //arrayStack.Push(1);
+            //arrayStack.Push(2);
+            //arrayStack.Push(3);
+            //arrayStack.Push(4);
+            //arrayStack.Push(5);
+
+            //while (arrayStack.Count > 0)
+            //{
+            //    Console.WriteLine(arrayStack.Pop());
+            //}
+        }
+    }
+}
